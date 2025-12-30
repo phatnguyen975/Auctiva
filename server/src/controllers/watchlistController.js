1449114ab6bb3bb2cfad26b7bb460992240491b8
Phat Nguyen <phatnguyen9725@gmail.com>
2025-12-31 05:01:00 +0700

    Setup for watchlist api

---
+++ server/src/controllers/watchlistController.js
@@ -0,0 +1,39 @@
+import WatchlistService from "../services/watchlistService.js";
+
+const WatchlistController = {
+  create: async (req, res) => {
+    try {
+      const productId = Number(req.validated.params.id);
+      const userId = req.user.id;
+      const newWatchlist = await WatchlistService.addProductToWatchlist({
+        productId,
+        userId,
+      });
+      res.created("Product added to watchlist successfully", newWatchlist);
+    } catch (error) {
+      res.error(error.message);
+    }
+  },
+
+  getByUserId: async (req, res) => {
+    try {
+      const userId = req.user.id;
+      const watchlist = await WatchlistService.getWatchlistByUserId(userId);
+      res.ok("Watchlist retrieved successfully", watchlist);
+    } catch (error) {
+      res.error(error.message);
+    }
+  },
+
+  delete: async (req, res) => {
+    try {
+      const productId = Number(req.validated.params.id);
+      await WatchlistService.deleteProductFromWatchlist(productId);
+      res.ok("Product deleted from watchlist successfully", null);
+    } catch (error) {
+      res.error(error.message);
+    }
+  },
+};
+
+export default WatchlistController;
---
+++ server/src/services/watchlistService.js
@@ -0,0 +1,17 @@
+import { prisma } from "../configs/prisma.js";
+
+const WatchlistService = {
+  addProductToWatchlist: async ({ productId, userId }) => {
+    
+  },
+
+  getWatchlistByUserId: async (userId) => {
+    
+  },
+
+  deleteProductFromWatchlist: async (productId) => {
+    
+  },
+};
+
+export default WatchlistService;
